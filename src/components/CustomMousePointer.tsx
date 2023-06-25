3415e7d0de58100d5a0104af90cae267c8707486
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-25 19:45:07 +0530

    chor: function added to set variable realtime

--- src/components/CustomMousePointer.tsx
+++ src/components/CustomMousePointer.tsx
@@ -1,10 +1,22 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import MousePointer from "../assets/custom-mouse.png";
-import React from "react";
+import React, { useEffect } from "react";
 
 function CustomMousePointer(props: any) {
+  const [valx, setValx] = React.useState(0);
+  const [valy, setValy] = React.useState(0);
+  const [title, setTitle] = React.useState("");
+
+  useEffect(() => {
+    props.socket.on("sendDatatoClient", (dataFromServer: any) => {
+      setValx(dataFromServer.position.x);
+      setValy(dataFromServer.position.y);
+      setTitle(dataFromServer.user.username);
+    });
+  }, [props.socket]);
+
   return (
-    <Box position={"relative"}>
+    <Box position={"relative"} top={valx} left={valy}>
       <Box position={"absolute"}>
         <Flex
           color={"white"}
@@ -14,7 +26,7 @@ function CustomMousePointer(props: any) {
           m={2}
         >
           <Image src={MousePointer} alt="custom-mouse" />
-          <Text fontWeight={"bold"}>Name goes Here from props</Text>
+          <Text fontWeight={"bold"}>{title}</Text>
         </Flex>
       </Box>
     </Box>
